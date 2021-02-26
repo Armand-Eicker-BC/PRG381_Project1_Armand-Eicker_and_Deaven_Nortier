5d92e4e3ebac4a23bf11495c168211b77839b4e7
Armand-Eicker <50994908+Armand-Eicker@users.noreply.github.com>
2021-02-26 15:20:31 +0200

    Create Table Script

--- Booking_System/BusinessLogicLayer/Customer.java
+++ Booking_System/BusinessLogicLayer/Customer.java
@@ -2,18 +2,26 @@ package Booking_System.BusinessLogicLayer;
 
 public class Customer {
     //Declaring encapsulated attributes
+    private String title;
     private String custName;
     private String custSurname;
+    private String email;
     private int phoneNum;
 
     //Constructor
-    public Customer(String N,String S,int Num){
+    public Customer(String T, String N,String S,String E,int Num){
+        this.title =T;
         this.custName = N;
         this.custSurname = S;
+        this.email =E;
         this.phoneNum = Num;
     }
 
     //Creating Get Methods
+    public String GetTitle(){
+        return this.title;
+    }
+
     public String GetName(){
         return this.custName;
     }
@@ -22,17 +30,29 @@ public class Customer {
         return this.custSurname;
     }
 
+    public String GetMail(){
+        return this.email;
+    }
+
     public int GetNum(){
         return this.phoneNum;
     }
 
     //Creating Set Methods
+    public void SetTitle(String title){
+        this.title = title;
+    }
+
     public void SetName(String name){
         this.custName = name;
     }
 
     public void SetSName(String sname){
         this.custSurname = sname;
+    }
+
+    public void SetMail(String mail){
+        this.email = mail;
     }
 
     public void SetNum(int num){
