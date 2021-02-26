6e70fba87437ab9286847aabbe77f8f7a4bb9866
Armand-Eicker <50994908+Armand-Eicker@users.noreply.github.com>
2021-02-26 14:40:28 +0200

    Update BusinessOperations.java

--- Booking_System/BusinessLogicLayer/BusinessOperations.java
+++ Booking_System/BusinessLogicLayer/BusinessOperations.java
@@ -2,6 +2,7 @@ package Booking_System.BusinessLogicLayer;
 
 public class BusinessOperations {
 
+    //Checks if discount should be applied
     public boolean checkDiscount(int numOfPeople){
         boolean check = false;
 
@@ -17,6 +18,7 @@ public class BusinessOperations {
         return check;
     }
 
+    //calculates a discounted price at 15% of the amount
     public double applyDiscount(float amount){
         double discountAmount = amount - (amount * 0.15);
         return discountAmount;
