1f8cb64880f616794095da7c0c74f2a311ebe7b0
Armand-Eicker <50994908+Armand-Eicker@users.noreply.github.com>
2021-02-26 14:30:44 +0200

    Update Customer.java

--- Booking_System/BusinessLogicLayer/Customer.java
+++ Booking_System/BusinessLogicLayer/Customer.java
@@ -1,7 +1,41 @@
 package Booking_System.BusinessLogicLayer;
 
 public class Customer {
-    String custName;
-    String custSurname;
-    int phoneNum;
+    //Declaring encapsulated attributes
+    private String custName;
+    private String custSurname;
+    private int phoneNum;
+
+    //Constructor
+    public Customer(String N,String S,int Num){
+        this.custName = N;
+        this.custSurname = S;
+        this.phoneNum = Num;
+    }
+
+    //Creating Get Methods
+    public String GetName(){
+        return this.custName;
+    }
+
+    public String GetSName(){
+        return this.custSurname;
+    }
+
+    public int GetNum(){
+        return this.phoneNum;
+    }
+
+    //Creating Set Methods
+    public void SetName(String name){
+        this.custName = name;
+    }
+
+    public void SetSName(String sname){
+        this.custSurname = sname;
+    }
+
+    public void SetNum(int num){
+        this.phoneNum = num;
+    }
 }
