c246b5dc97eda89d33be7ac70133e3dfce3572cb
Armand-Eicker <50994908+Armand-Eicker@users.noreply.github.com>
2021-02-26 17:56:18 +0200

    Update Datahandler.java

--- Booking_System/DataAccessLayer/Datahandler.java
+++ Booking_System/DataAccessLayer/Datahandler.java
@@ -1,5 +1,29 @@
 package Booking_System.DataAccessLayer;
 
+import java.io.IOException;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.*;
+
+import Booking_System.BusinessLogicLayer.Customer;
+
 public class Datahandler {
-    //g
+       
+    String sqlQ;
+
+    public void RegisterCustomer(Customer cust)throws IOException{
+        try 
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/my_students","root","");
+            Statement stmt=con.createStatement();
+            sqlQ = "INSERT INTO Customers VALUES("+ cust.GetTitle() +","+ cust.GetName() +"," + cust.GetSName() +"," + cust.GetNum() +"," + cust.GetMail() +")";
+            stmt.executeQuery(sqlQ);
+        } 
+        catch (Exception e) 
+        {
+            //TODO: handle exception
+        }
+    }
 }
